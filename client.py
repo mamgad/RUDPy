a97f143d3a991334eb26ab62fde8c810b6387cc4
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-03 18:41:55 +0200

    Multiple fixes and changes

--- client.py
+++ client.py
@@ -1,35 +1,29 @@
 import socket
 import sys
 
-
-
-
-# Create a UDP socket
+# Start - Connection initiation
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = ('localhost', 10000)
-
-userInput=raw_input("File to be requested:")
+userInput=raw_input("Requested file: ")
 message=userInput;
 
 try:
-
     # Send data
-    print  'sending "%s"' % message
+    print  'Requesting %s' % message
     sent = sock.sendto(message, server_address)
+    # Receive indefinitely
     while(1):
     # Receive response
-        print  'waiting to receive'
+        print  'Waiting to receive..'
         data, server = sock.recvfrom(4096)
-
-        print  'received "%s"' % data
+        print  'Received: %s' % data
         seqNo = data.split(",")[1]
         packetLength = data.split(",")[2]
-        print "seqNo = %s len is %s" % (seqNo, packetLength);
-        print server;
+        print "Sequence number: %s\nLength: %s" % (seqNo, packetLength);
+        print "Server: %s\nPort: %s" % server;
         sent = sock.sendto(str(seqNo) + "," + packetLength, server)
 
 
 finally:
-    print >>sys.stderr, 'closing socket'
+    print >> sys.stderr, 'Closing socket'
     sock.close()
--- server.py
+++ server.py
@@ -3,95 +3,66 @@ import sys
 import threading
 
 
-#Seq number flag
+# Seq number flag
 seqFlag=0
 
-
+# Packet class definition
 class packet():
-    checksum =0;
+    checksum=0;
     length=0;
     seqNo=0;
     msg=0;
-
     def make(self,data):
         self.msg = data
-        self.length = str(len(data)).zfill(3)
+        self.length = str(len(data))
+        print "Message: %s\nLength: %s\nSequence number: %s" % (self.msg, self.length, self.seqNo)
 
-        print "Message is %s length is %s seqNo is %s" % (self.msg, self.length, self.seqNo)
-
-class ResponseThread(threading.Thread):
-    def __init__(self, data, address):
-        threading.Thread.__init__(self)
-        self.data = data
-        self.address = address
-    def run(self):
-        print "thread data is %s"% self.data;
-
-
+# Connection handler
 def handleConnection(address,data):
-
     threadSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     thread_server_address = ('localhost', 10001)
     threadSock.bind(thread_server_address)
     try:
-        print 'Opening file %s'%data
+        # Read requested file
+        print 'Opening file %s' % data
         fileRead=open(data , 'r')
         data = fileRead.read()
-
         fileRead.close()
-
-        #print "content length is data %s" % pkt.length;
+                
+        # Fragment and send file 500 byte by 500 byte
         x = 0
-        #Fragment and send file 500 byte by 500 byte
-        for x in range(0, (len(data) / 500)+1):
-            print data[x * 500:x * 500 + 500]
+        for x in range(0, (len(data) / 500) + 1):
             msg = data[x * 500:x * 500 + 500];
             pkt.make(msg);
-            print "length is %s" % str(pkt.length);
-
             finalPacket = str(pkt.checksum)+","+str(pkt.seqNo)+","+str(pkt.length)+","+pkt.msg
-
-            sent = threadSock.sendto(finalPacket, address) #send packet
-            print  'sent %s bytes back to %s waiting for ack..' % (sent, address)
+            
+            # Send packet
+            sent = threadSock.sendto(finalPacket, address) 
+            print  'Sent %s bytes back to %s, awaiting acknowledgment..' % (sent, address)
             ack, address = threadSock.recvfrom(100);
-            print "first is %s and second is %s"%(ack.split(",")[0],pkt.seqNo)
             if (ack.split(",")[0]==str(pkt.seqNo)):
                 pkt.seqNo=(pkt.seqNo+1)%2
-                print "\n\n\n\n\nS NUMBER IS NOW %s\n\n\n\n\n"%str(pkt.seqNo);
-            print(ack)
-            x = +1
-
-
-        #fileRead.close();
-
-
-    except:#fail opening file
-        print "error opening file"
-        exit()
+            print("Acknowledgment: " + ack)
+            x += 1
+    
+    # File opening failure handling
+    except:
+        print "Error on opening the requested file"
 
 
 
-
+# Start - Connection initiation
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print  'starting up on %s port %s' % server_address
+print  'Starting up on %s port %s' % server_address
 sock.bind(server_address)
 pkt=packet()
 
-
+# Listening for requests indefinitely
 while True:
-    print  '\nwaiting to receive message'
+    print  '\nWaiting to receive message'
     data, address = sock.recvfrom(600)
-    #handleConnection(address,data);
-
-
     connectionThread = threading.Thread(target=handleConnection, args=(address, data))
-
     connectionThread.start()
-
-
-
-    print  'received %s bytes from %s' % (len(data), address)
-    #print  data
+    print  'Received %s bytes from %s' % (len(data), address)
