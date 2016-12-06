bfdfceac49e6f9efd096d22a8e9db25e112242ef
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-06 09:01:20 +0200

    Handle transmission of large files

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ delimiter = "|:|:|";
 # Start - Connection initiation
 while 1:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(2);
+    sock.settimeout(10);
     server_address = ('localhost', 10000)
     userInput = raw_input("\nRequested file: ")
     message = userInput;
